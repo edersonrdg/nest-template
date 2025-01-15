79f74c82bd7dfcf4c716cdd5169f797b6b9165ee
Ederson <ederson.lopes@enacom.com.br>
2025-01-14 22:36:18 -0300

    feat: ensure prisma client global

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,12 +5,14 @@ import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
     UserModule,
     ProductModule,
     OrderModule,
