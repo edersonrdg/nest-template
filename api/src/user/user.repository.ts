30d95ad855e3e8f19c08fefea03c7de475f70cc3
Ederson <ederson.lopes@enacom.com.br>
2025-01-15 10:15:19 -0300

    feat: get user by email

--- api/src/user/user.repository.ts
+++ api/src/user/user.repository.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { User } from './entities/user.entity';
 
 export interface UserRepository {
-  create(data: CreateUserDto): Promise<any>;
+  create(data: CreateUserDto): Promise<User>;
+  getUserByEmail(email: string): Promise<User>;
 }
 
 @Injectable()
 export class UserRepositoryPrismaDB implements UserRepository {
   constructor(private prismaService: PrismaService) {}
 
-  async create(data: CreateUserDto): Promise<any> {
-    await this.prismaService.user.create({ data });
+  async create(data: CreateUserDto): Promise<User> {
+    return await this.prismaService.user.create({ data });
+  }
+
+  async getUserByEmail(email: string): Promise<User> {
+    return await this.prismaService.user.findFirst({
+      where: { email },
+    });
   }
 }
