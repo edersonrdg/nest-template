fa98d0fa964db2d5bd7f66370b5afd11a9ff4582
Ederson <ederson.lopes@enacom.com.br>
2025-01-16 09:53:59 -0300

    feat: add auth-guard method

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import * as bcrypt from 'bcrypt';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
