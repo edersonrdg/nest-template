dbd4b6692f6f00e6ce28dc2e9ae9a23a13682046
Ederson <ederson.lopes@enacom.com.br>
2025-01-16 13:16:47 -0300

    fix product test error

--- api/src/product/product.controller.spec.ts
+++ api/src/product/product.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductController } from './product.controller';
+import { CreateProductUseCase } from './usecases/create-product.usecase';
+import { GetAllProductUseCase } from './usecases/get-all.usecase';
+import { GetOneProductUseCase } from './usecases/get-one.usecase';
+import { UpdateProductUseCase } from './usecases/update.usecase';
+import { DeleteProductUseCase } from './usecases/delete.usecase';
+import { ProductRepositoryPrismaDB } from './product.repository';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('ProductController', () => {
   let controller: ProductController;
@@ -7,7 +15,16 @@ describe('ProductController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductController],
-      providers: [],
+      providers: [
+        JwtService,
+        PrismaService,
+        ProductRepositoryPrismaDB,
+        CreateProductUseCase,
+        GetAllProductUseCase,
+        GetOneProductUseCase,
+        UpdateProductUseCase,
+        DeleteProductUseCase,
+      ],
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
