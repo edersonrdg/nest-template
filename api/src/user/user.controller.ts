384ffe5b9e4fb5141f120f3d8acce4e029ba52ee
Ederson <ederson.lopes@enacom.com.br>
2025-01-16 10:01:02 -0300

    feat: ensure auth guard global

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { CreateUserUseCase } from './usecases/create-user.usecase';
-import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.createUserUseCase.execute(createUserDto);
--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { CreateUserUseCase } from './usecases/create-user.usecase';
 import { UserRepositoryPrismaDB } from './user.repository';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Module({
   controllers: [UserController],
-  providers: [UserRepositoryPrismaDB, CreateUserUseCase],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    UserRepositoryPrismaDB,
+    CreateUserUseCase,
+  ],
 })
 export class UserModule {}
