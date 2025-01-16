af0e2208b9874c09fbe21df32c9f1ef06c401c5a
Ederson <ederson.lopes@enacom.com.br>
2025-01-16 11:06:45 -0300

    feat: email already used exception

--- api/src/user/dto/create-user.dto.ts
+++ api/src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { Role } from '@prisma/client';
+import { Role } from '../../auth/roles/roles.enum';
 
 export class CreateUserDto {
   name: string;
--- api/src/user/usecases/create-user.usecase.ts
+++ api/src/user/usecases/create-user.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRepositoryPrismaDB } from '../user.repository';
 import * as bcrypt from 'bcrypt';
@@ -7,6 +7,10 @@ import * as bcrypt from 'bcrypt';
 export class CreateUserUseCase {
   constructor(private userRepository: UserRepositoryPrismaDB) {}
   async execute(createUserDto: CreateUserDto) {
+    if (await this.userRepository.getUserByEmail(createUserDto.email)) {
+      throw new BadRequestException('Email already used');
+    }
+
     createUserDto.password = bcrypt.hashSync(createUserDto.password, 10);
     return await this.userRepository.create(createUserDto);
   }
