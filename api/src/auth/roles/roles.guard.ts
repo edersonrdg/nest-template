f466609c6922d5796255eff82ef565b751a1e837
Ederson <ederson.lopes@enacom.com.br>
2025-01-16 13:07:56 -0300

    remove unused func

--- api/src/auth/roles/roles.guard.ts
+++ api/src/auth/roles/roles.guard.ts
@@ -1,9 +1,4 @@
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  Inject,
-} from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from './roles.decorator';
 import { Role } from './roles.enum';
