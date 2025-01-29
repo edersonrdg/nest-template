6bb5f8af72fdb6e940b8eee23d05cdf76f143b04
Ederson <edersonrodrigo31@gmail.com>
2025-01-29 10:02:28 -0300

    refactor: revert usecase to service user

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,19 +1,21 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { CreateUserUseCase } from './usecases/create-user.usecase';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RolesGuard } from 'src/auth/roles/roles.guard';
 import { Role } from '../auth/roles/roles.enum';
 import { Roles } from '../auth/roles/roles.decorator';
+import { UserService } from './user.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  constructor(private userService: UserService) {}
 
+  @ApiBearerAuth()
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.createUserUseCase.execute(createUserDto);
+    return this.userService.signUp(createUserDto);
   }
 }
--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-import { CreateUserUseCase } from './usecases/create-user.usecase';
 import { UserRepositoryPrismaDB } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserRepositoryPrismaDB, CreateUserUseCase],
+  providers: [UserRepositoryPrismaDB, UserService],
   exports: [],
 })
 export class UserModule {}
