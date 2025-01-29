a699caa8110584635423d6629717eec0e8e1e979
Ederson <edersonrodrigo31@gmail.com>
2025-01-29 09:36:16 -0300

    chore: add swagger

---
+++ api/src/docs.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const swaggerConfig = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Stock System')
+    .setDescription('Inventory System API')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+};
