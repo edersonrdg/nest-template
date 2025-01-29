728f076a97d81a56f1db1d56e2923f82def509e3
Ederson <edersonrodrigo31@gmail.com>
2025-01-29 09:36:33 -0300

    feat: apply swagger config

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './logger/interceptor';
+import { swaggerConfig } from './docs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor());
+  swaggerConfig(app);
   await app.listen(3000);
 }
 bootstrap();
