c0977b2fe5bfc41652bf39c5cad774361c87a4ed
Ederson <edersonrodrigo31@gmail.com>
2025-01-30 09:38:27 -0300

    feat: update user validation

--- api/src/user/dto/update-user.dto.ts
+++ api/src/user/dto/update-user.dto.ts
@@ -1,4 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    example: 'John Doe',
+    description: 'user name',
+  })
+  @IsString()
+  @MinLength(3)
+  name?: string;
+
+  @ApiProperty({
+    example: 'user@user.com',
+    description: 'user email',
+    format: 'email',
+  })
+  @IsEmail()
+  @IsString()
+  email?: string;
+}
