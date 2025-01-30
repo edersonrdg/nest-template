66f6209a1d382a70c582aba96982af97e0eef8c8
Ederson <edersonrodrigo31@gmail.com>
2025-01-30 09:49:52 -0300

    feat: update product validation

--- api/src/product/dto/update-product.dto.ts
+++ api/src/product/dto/update-product.dto.ts
@@ -1,4 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, Min, MinLength } from 'class-validator';
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @ApiProperty({
+    example: 'product',
+    description: 'Product name',
+    format: 'string',
+  })
+  @IsString()
+  @MinLength(3)
+  ref?: string;
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+  @ApiProperty({
+    example: 10,
+    description: 'Product price',
+    format: 'number',
+  })
+  @Min(0.01)
+  @IsNumber({ maxDecimalPlaces: 2 })
+  price?: number;
+}
