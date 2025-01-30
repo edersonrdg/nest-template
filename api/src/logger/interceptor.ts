10d89b13249684c64d62cf0f07b950cbea2431a6
Ederson <edersonrodrigo31@gmail.com>
2025-01-30 08:43:46 -0300

    feat: application logs

--- api/src/logger/interceptor.ts
+++ api/src/logger/interceptor.ts
@@ -3,24 +3,28 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  Logger,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const req = context.switchToHttp().getRequest();
 
+    const requestLog = new Logger(`ApplicationRequest`);
     const now = Date.now();
-    return next
-      .handle()
-      .pipe(
-        tap(() =>
-          console.log(
-            `[${new Date().toISOString()}] ${req.method} ${req.url} ${Date.now() - now}ms`,
-          ),
-        ),
-      );
+    return next.handle().pipe(
+      tap(() =>
+        requestLog.log(`${req.method} ${req.url} ${Date.now() - now}ms`),
+      ),
+      catchError((error) => {
+        requestLog.error(
+          `${req.method} ${req.url} ${Date.now() - now}ms - Error: ${error.message}`,
+        );
+        throw error;
+      }),
+    );
   }
 }
